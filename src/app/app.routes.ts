06c3370aff1b070dc7198bf645a0aefb558b0f7d
Matheus Castilho <Matheus.Castilho@jala.university>
2025-06-03 15:18:17 -0300

    Add: add a register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { HomeComponent } from './components/home/home.component';
 import { CatalogComponent } from './components/catalog/catalog.component';
 import { provideRouter } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes = [
   { path: '', component: HomeComponent },
   { path: 'catalog', component: CatalogComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component:  RegisterComponent}
+
 ];
 
 bootstrapApplication(HomeComponent, {
