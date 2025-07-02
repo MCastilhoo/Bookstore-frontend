a888533613fd06fa283a64bd9d8d4a43ba8aa1b1
Matheus Castilho <dev.mcastilho@gmail.com>
2025-07-02 15:30:17 -0300

    [FIX]: Fixed routes bug when registering a user.

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -3,6 +3,7 @@ import { Router, RouterModule } from "@angular/router";
 import { AuthService } from "../../services/auth.service";
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
+import { User } from "../../interface/user-interface";
 
 @Component({
     selector: 'app-register',
@@ -21,7 +22,7 @@ export class RegisterComponent {
     constructor(private authService: AuthService, private router: Router) { }
 
     register() {
-        this.apiError = null; 
+        this.apiError = null;
         if (!this.firstName || !this.lastName || !this.userEmail || !this.password) {
             return;
         }
@@ -29,23 +30,18 @@ export class RegisterComponent {
         this.authService
             .register(this.firstName, this.lastName, this.userEmail, this.password)
             .subscribe({
-                next: () => {
-                    const newUser ={
-                        firstName: this.firstName,
-                        lastName: this.lastName,
-                        userEmail: this.userEmail
-                    };
-
-                    this.authService.userSignal.set(newUser);
-                    localStorage.setItem('user', JSON.stringify(newUser))
-                    this.router.navigate(['/login']);
+                next: (user: User) => {
+                    this.authService.userSignal.set(user);
+                    localStorage.setItem('user', JSON.stringify(user)); 
+                    this.router.navigate(['/email-confirmation']);
                 },
                 error: (err) => {
                     if (err.status === 409) {
-                        this.apiError = "Este e-mail já foi cadastrado. Por favor use outro ou faça o login."
+                        this.apiError = "Este e-mail já foi cadastrado. Por favor use outro ou faça o login.";
+                    } else {
+                        this.apiError = "Erro ao registrar. Tente novamente mais tarde.";
                     }
                 }
             });
     }
-
 }
