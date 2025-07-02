1649a484fda9e769790e500383a0b3d9b471944e
Matheus Castilho <dev.mcastilho@gmail.com>
2025-07-02 15:38:43 -0300

    [FIX]: Fixed the logic so that only administrators access the book creation page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,19 @@
+import { AdminGuard } from './guard/admin.guard';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { HomeComponent } from './components/home/home.component';
 import { CatalogComponent } from './components/catalog/catalog.component';
 import { provideRouter } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { CreateBookComponent } from './create-book/create-book.component';
+import { CreateBookComponent } from './components/create-book/create-book.component';
+import { EmailConfirmationComponent } from './components/email-confirmation/email-confirmation.component';
 
 export const routes = [
   { path: '', component: HomeComponent },
   { path: 'catalog', component: CatalogComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component:  RegisterComponent},
-  { path: 'create-book', component: CreateBookComponent }
+  { path: 'create-book', component: CreateBookComponent, canActivate:[AdminGuard] },
+  { path: 'email-confirmation', component: EmailConfirmationComponent }
 
 ];
