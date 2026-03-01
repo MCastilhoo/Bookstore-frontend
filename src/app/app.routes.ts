2ac55668c2e9d6812bd479322c298fd809c1f1c1
Matheus Castilho <dev.mcastilho@gmail.com>
2026-03-01 09:55:55 -0300

    [FEAT]: transition login and registration to modal components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { HomeComponent } from './pages/home/home.component';
 import { CatalogComponent } from './pages/catalog/catalog.component';
 import { provideRouter } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
 import { EmailConfirmationComponent } from './pages/email-confirmation/email-confirmation.component';
 
 export const routes = [
   { path: '', component: HomeComponent },
   { path: 'catalog', component: CatalogComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component:  RegisterComponent},
   { path: 'email-confirmation', component: EmailConfirmationComponent }
 
 ];
