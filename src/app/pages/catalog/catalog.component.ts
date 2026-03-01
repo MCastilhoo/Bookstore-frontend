c0dc4abde7cf182e3be25050dfc0cddb98ec35ea
Matheus Castilho <dev.mcastilho@gmail.com>
2026-03-01 12:10:51 -0300

    [FEAT]: implement book card component (static version).

--- src/app/pages/catalog/catalog.component.ts
+++ src/app/pages/catalog/catalog.component.ts
@@ -2,12 +2,14 @@ import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { RegisterBookModalComponent } from '../../components/register-book-modal/register-book-modal.component';
+import { BookCardComponent } from '../../components/book-card/book-card.component';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, BookCardComponent],
   templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent {
   private dialog = inject(MatDialog);
