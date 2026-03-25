48a29b037984d62539ff4361cf66faf12594e369
Matheus Castilho <dev.mcastilho@gmail.com>
2026-03-25 01:30:12 -0300

    feat: integrate book card with backend.

--- src/app/components/book-card/book-card.component.ts
+++ src/app/components/book-card/book-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Book } from '../../interface/book-interface';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-book-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './book-card.component.html',
   styleUrl: './book-card.component.css'
 })
